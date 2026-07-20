a1b7b20795bd8d452b4ee15b07957a114c5a1f26
qwer0114 <rnalgh0114@gmail.com>
2026-07-20 11:18:29 +0900

    feat: 신규 구독 등록 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./routes/landing";
 import LoginPage from "./routes/login";
 import HomePage from "./routes/home";
+import SubscribeNewPage from "./routes/subscribe/new";
 
 function App() {
   return (
-    <div className="app-scroll max-w-120 overflow-auto m-auto h-dvh bg-white shadow-[0_0_20px_#0000000d]">
+    <div
+      id="app-root"
+      className="app-scroll max-w-120 overflow-auto m-auto h-dvh bg-white shadow-[0_0_20px_#0000000d] contain-layout"
+    >
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/subscribe/new" element={<SubscribeNewPage />} />
       </Routes>
     </div>
   );
